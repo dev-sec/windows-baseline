89d4f9f31ca80878b21fc2f769bc16e38b1e7618
Dominik Richter <dominik.richter@gmail.com>
2015-10-26 17:00:44 +0100

    use inspec registry_key resources

--- test/base_spec.rb
+++ test/base_spec.rb
@@ -23,7 +23,7 @@ rule "windows-base-101" do
     @link: https://msdn.microsoft.com/en-us/library/ms682586(v=vs.85).aspx
     @link: https://technet.microsoft.com/en-us/library/dd277307.aspx
   "
-  describe windows_registry_key("HKLM\\System\\CurrentControlSet\\Control\\Session Manager") do
+  describe registry_key("HKLM\\System\\CurrentControlSet\\Control\\Session Manager") do
     it { should exist }
     it { should_not have_property_value("SafeDllSearchMode", :type_dword, "0") }
   end
