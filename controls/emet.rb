0d55ed72d4d2c89cfe95c7218f73e5942cbf9cfe
juju4 <juju4@users.noreply.github.com>
2017-05-28 14:54:50 -0400

    stig reference for EMET

--- controls/emet.rb
+++ controls/emet.rb
@@ -10,6 +10,7 @@ if emet_present
     title 'Ms EMET is running'
     desc 'EMET process monitoring is active'
     ref url: 'https://insights.sei.cmu.edu/cert/2016/11/windows-10-cannot-protect-insecure-applications-like-emet-can.html'
+    ref url: 'https://www.stigviewer.com/stig/windows_8_8.1/2014-06-27/finding/V-39137'
     describe processes('EMET_Service.exe') do
       its('list.length') { should eq 1 }
       its('users') { should cmp 'SYSTEM' }
