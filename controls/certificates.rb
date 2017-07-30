424818fb80a191695add253ed5446704478ac42e
juju4 <juju4@users.noreply.github.com>
2017-07-30 11:18:33 -0400

    ruby array space escape

--- controls/certificates.rb
+++ controls/certificates.rb
@@ -3,63 +3,63 @@
 windows_certificates_nogov = attribute('windows_certificates_nogov', default: true, description: 'Ensure no certificates from a governmental entity')
 ## from Windows Participants: https://gallery.technet.microsoft.com/Trusted-Root-Certificate-123665ca
 windows_certificates_nogov_list = %w(
-  'ACCVRAIZ1'
-  'AC RAIZ FNMT-RCM'
-  'AC1 RAIZ MTIN'
-  'Australian Defence Organisation (ADO) Certificate Authority 02'
-  'Australian Defence Public Root CA'
-  'Autoridad Certificadora Raíz Nacional de Uruguay'
-  'Autoridad de Certificacion de la Abogacia'
-  'Autoridad de Certificacion Firmaprofesional CIF A62634068'
-  'Autoridad de Certificacion Raiz de la Republica Bolivariana de Venezuela'
-  'Autoridad de Certificacion Raiz de la Republica Bolivariana de Venezuela 1'
-  'Autoridade Certificadora da Raiz Brasileira v1 - ICP-Brasil'
-  'Autoridade Certificadora Raiz Brasileira v2'
-  'CCA India 2014'
-  'CCA India 2015'
-  'Certinomis - Autorité Racine'
-  'Certinomis - Root CA'
-  'Common Policy'
-  'Correo Uruguayo - Root CA'
-  'DIRECCION GENERAL DE LA POLICIA'
-  'E-ME SSI (RCA)'
-  'ECRaizEstado'
-  'Fabrica Nacional de Moneda y Timbre'
-  'Federal Government Common Policy'
-  'Fotanúsítványkiadó - Kormányzati Hitelesítés Szolgáltató'
-  'Government of Netherlands G3'
-  'Government of Sweden (Försäkringskassan)'
-  'Government Root Certification Authority - Taiwan'
-  'GPKI ApplicationCA2 Root'
-  'GPKIRootCA1'
-  'KISA RootCA 1'
-  'Hongkong Post Root CA 1'
-  'Macao Post eSign Trust'
-  'Posta CA Root'
-  'POSTarCA'
-  'PostSignum Root QCA 2'
-  'Saudi National Root CA'
-  'SAPO Class 2 Root CA'
-  'SAPO Class 3 Root CA'
-  'SAPO Class 4 Root CA'
-  'Secrétariat Général de la Défense Nationale'
-  'SI-TRUST Root'
-  'Staat der Nederlanden EV Root CA'
-  'Staat der Nederlanden Root CA - G2'
-  'Swedish Government Root Authority v1'
-  'Swedish Government Root Authority v3'
-  'Swiss Government Root CA I'
-  'Swiss Government Root CA II'
-  'Swiss Government Root III'
-  'Thailand National Root Certification Authority - G1'
-  'TÜBITAK Kamu SM'
-  'TUBITAK Kamu SM SSL Kok Sertifikasi - Surum 1'
-  'Tunisian Root Certificate Authority - TunRootCA2'
-  'TW Government Root Certification Authority'
-  'TW Government Root Certification Authority 2'
-  'VRK Gov. Root CA'
-  'VAS Latvijas Pasts SSI(RCA)'
-  'VI Registru Centras'
+  ACCVRAIZ1
+  AC\ RAIZ\ FNMT-RCM
+  AC1\ RAIZ\ MTIN
+  Australian\ Defence\ Organisation\ (ADO)\ Certificate\ Authority\ 02
+  Australian\ Defence\ Public\ Root\ CA
+  Autoridad\ Certificadora\ Raíz\ Nacional\ de\ Uruguay
+  Autoridad\ de\ Certificacion\ de\ la\ Abogacia
+  Autoridad\ de\ Certificacion\ Firmaprofesional\ CIF\ A62634068
+  Autoridad\ de\ Certificacion\ Raiz\ de\ la\ Republica\ Bolivariana\ de\ Venezuela
+  Autoridad\ de\ Certificacion\ Raiz\ de\ la\ Republica\ Bolivariana\ de\ Venezuela\ 1
+  Autoridade\ Certificadora\ da\ Raiz\ Brasileira\ v1\ -\ ICP-Brasil
+  Autoridade\ Certificadora\ Raiz\ Brasileira\ v2
+  CCA\ India\ 2014
+  CCA\ India\ 2015
+  Certinomis\ -\ Autorité\ Racine
+  Certinomis\ -\ Root\ CA
+  Common\ Policy
+  Correo\ Uruguayo\ -\ Root\ CA
+  DIRECCION\ GENERAL\ DE\ LA\ POLICIA
+  E-ME\ SSI\ (RCA)
+  ECRaizEstado
+  Fabrica\ Nacional\ de\ Moneda\ y\ Timbre
+  Federal\ Government\ Common\ Policy
+  Fotanúsítványkiadó\ -\ Kormányzati\ Hitelesítés\ Szolgáltató
+  Government\ of\ Netherlands\ G3
+  Government\ of\ Sweden\ (Försäkringskassan)
+  Government\ Root\ Certification\ Authority\ -\ Taiwan
+  GPKI\ ApplicationCA2\ Root
+  GPKIRootCA1
+  KISA\ RootCA\ 1
+  Hongkong\ Post\ Root\ CA\ 1
+  Macao\ Post\ eSign\ Trust
+  Posta\ CA\ Root
+  POSTarCA
+  PostSignum\ Root\ QCA\ 2
+  Saudi\ National\ Root\ CA
+  SAPO\ Class\ 2\ Root\ CA
+  SAPO\ Class\ 3\ Root\ CA
+  SAPO\ Class\ 4\ Root\ CA
+  Secrétariat\ Général\ de\ la\ Défense\ Nationale
+  SI-TRUST\ Root
+  Staat\ der\ Nederlanden\ EV\ Root\ CA
+  Staat\ der\ Nederlanden\ Root\ CA\ -\ G2
+  Swedish\ Government\ Root\ Authority\ v1
+  Swedish\ Government\ Root\ Authority\ v3
+  Swiss\ Government\ Root\ CA\ I
+  Swiss\ Government\ Root\ CA\ II
+  Swiss\ Government\ Root\ III
+  Thailand\ National\ Root\ Certification\ Authority\ -\ G1
+  TÜBITAK\ Kamu\ SM
+  TUBITAK\ Kamu\ SM\ SSL\ Kok\ Sertifikasi\ -\ Surum\ 1
+  Tunisian\ Root\ Certificate\ Authority\ -\ TunRootCA2
+  TW\ Government\ Root\ Certification\ Authority
+  TW\ Government\ Root\ Certification\ Authority\ 2
+  VRK\ Gov.\ Root\ CA
+  VAS\ Latvijas\ Pasts\ SSI(RCA)
+  VI\ Registru\ Centras
 )
 
 title 'Windows Certificates store'
